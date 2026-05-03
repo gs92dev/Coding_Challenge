0d8f1a8653a8deed2cc83a05ce5acab77bd5b22d
Guilherme dos Santos <gs92dev@gmail.com>
2026-05-03 18:15:35 -0500

    creating the put endpoint for the database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,70 +1,92 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import { Task } from './task-types.js'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import TaskModel from './task-model.js'
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import TaskModel from "./task-model.js";
+import { Task } from "./task-types.js";
 
-const app = express()
-const tasks: Array<Task> = [{
-  name: 'Default task',
-  due: new Date(Date.now() + 3600),
-  description: 'A default task from the backend',
-  complete: false
-}]
-const mongoURI = process.env.MONGODB_URI || 'mongodb://database:27017/tasks'
+const app = express();
+const tasks: Array<Task> = [
+  {
+    name: "Default task",
+    due: new Date(Date.now() + 3600),
+    description: "A default task from the backend",
+    complete: false,
+  },
+];
+const mongoURI = process.env.MONGODB_URI || "mongodb://database:27017/tasks";
 mongoose
   .connect(mongoURI)
-  .then(() => console.log('MongoDB connected.'))
-  .catch((err) => console.log(err))
+  .then(() => console.log("MongoDB connected."))
+  .catch((err) => console.log(err));
 
-
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:4200'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Set-Cookie'],
+    origin: ["http://localhost:4200"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "Set-Cookie",
+    ],
     credentials: true,
   }),
-)
+);
 
-app.get('/healthcheck', function(req, res) {
-  res.status(200).json({ success: true })
-  return
-})
+app.get("/healthcheck", function (req, res) {
+  res.status(200).json({ success: true });
+  return;
+});
 
-app.use(function(req, res, next) {
-  console.log(req.method + ' request at route ' + req.url)
-  next()
-})
+app.use(function (req, res, next) {
+  console.log(req.method + " request at route " + req.url);
+  next();
+});
 
-app.get('/api/tasks', async (req, res) => {
-  const tasks = await TaskModel.find()
-  res.json(tasks)
-  return
-})
+app.get("/api/tasks", async (req, res) => {
+  const tasks = await TaskModel.find();
+  res.json(tasks);
+  return;
+});
 
-app.post('/api/tasks', async (req, res) => {
-  const { task } = req.body
-  console.log(task)
-  await TaskModel.create(task)
-  res.status(201).send()
-  return
-})
+app.post("/api/tasks", async (req, res) => {
+  const { task } = req.body;
+  console.log(task);
+  await TaskModel.create(task);
+  res.status(201).send();
+  return;
+});
 
 /* Create your new route here */
 
-app.delete('/api/tasks/:id', async (req, res) => {
-  const { id } = req.params
+app.put("/api/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  const { task } = req.body;
   try {
-    await TaskModel.findByIdAndDelete(id)
-    res.status(204).send()
-    return
+    await TaskModel.findByIdAndUpdate(id, task);
+    res.status(204).send();
+    return;
   } catch (e) {
-    console.log(e)
-    res.status(404).send()
-    return
+    console.log(e);
+    res.status(404).send();
+    return;
   }
-})
+});
 
-app.listen(5200, () => console.log('Listening on port 5200'))
+app.delete("/api/tasks/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await TaskModel.findByIdAndDelete(id);
+    res.status(204).send();
+    return;
+  } catch (e) {
+    console.log(e);
+    res.status(404).send();
+    return;
+  }
+});
+
+app.listen(5200, () => console.log("Listening on port 5200"));
